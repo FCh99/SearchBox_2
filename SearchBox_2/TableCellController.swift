07c4427221d29a014bd87058c99820525165fc55
Fausto Checa <fausto.checa@gmail.com>
2017-10-19 13:07:39 +0200

    IOS

---
+++ SearchBox_2/TableCellController.swift
@@ -0,0 +1,27 @@
+//
+//  TableCellController.swift
+//  SearchBox_2
+//
+//  Created by Fausto Checa on 19/10/17.
+//  Copyright © 2017 Fausto Checa. All rights reserved.
+//
+
+import UIKit
+
+class TableCellController: UITableViewCell {
+    
+    @IBOutlet weak var label1: UILabel!
+    
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
